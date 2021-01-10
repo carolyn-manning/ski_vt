e18743d52cfa67797c74e2e0a68ff70f66c300fb
carolyn-manning <carolyn.manning3@gmail.com>
2021-01-09 21:56:15 -0500

    added code to scrape page

--- lib/ski_vt/scraper.rb
+++ lib/ski_vt/scraper.rb
@@ -11,8 +11,16 @@ class Scraper
         doc.css(".resort_conditions").each do |resort|
             name = resort.css(".name").text
             last_update = resort.css(".cond_info > :first-child").text
-           # profile_url = "#{student.css("a").attribute("href").value}"
-           # students << {name: name, location: location, profile_url: profile_url}
+            trails_open = resort.css(".trail_info .cond_info_value").text
+            lifts_open = resort.css(".lift_info .cond_info_value").text
+            surface = resort.css(".surface_cond .cond_info_value").text
+            status = resort.css(".res_cond_status .cond_info_value").text
+            last_snow = resort.css(".last_snow .cond_info_value").text
+            base = resort.css(".base_item .cond_info_value").text
+            24_hour_snow = resort.css(".hr_24 .cond_info_value").text
+            48_hour_snow = resort.css(".hr_48 .cond_info_value").text
+            report_link = resort.css(".cond_buttons :first-child").attribute("href").value
+            tickets_link = resort.css(".cond_buttons :nth-child(3)").attribute("href").value
         end 
         binding.pry
     end 
